e171416f902bbaf80320245d40a1e63398004c6b
ramazan260 <ramazan260@BPC06>
2023-12-12 08:35:37 +0300

    Proje dosyası ekle.

---
+++ yapıcı/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yapıcı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("program başladı");
+            Kisi k = new Kisi();
+            Console.WriteLine("adı={0},yaşı{1},k.yas,k.ad");
+            Console.WriteLine("Program bitti");
+        }
+    }
+    class Kisi
+    {
+        int yas;
+        string ad;
+        public Kisi()
+        {
+            yas = 19;
+            ad = "Ahmet";
+            Console.WriteLine("Yapıcı metot çalıştı.");
+        }
+        public int Yas
+        {
+            get
+            {
+                return yas;
+            }
+        }
+        public string Ad
+        {
+            get
+            {
+                return ad;
+            }
+        }
+    }
+}
